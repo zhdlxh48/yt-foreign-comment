9e2fd3613b4bab8d1ad9acf16d1ca7d2414800cf
zhdlxh48 <zhdlxh48@gmail.com>
2022-11-30 23:27:22 +0900

    Update comment edit util

--- pages/comment/[id].tsx
+++ pages/comment/[id].tsx
@@ -5,13 +5,19 @@ import Link from "next/link";
 import { ForeignComment } from "../../utils/language";
 import FileSaver from "file-saver";
 import { stringify } from "csv-stringify/sync";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type CommentProps = {
   comments: ForeignComment[];
 };
 const Comment: NextPage<CommentProps> = ({ comments }) => {
   const [cleanText, setCleanText] = useState<boolean>(false);
+
+  const [blurAvatar, setBlurAvatar] = useState<boolean>(false);
+  const [hideName, setHideName] = useState<boolean>(false);
+  const [hideDate, setHideDate] = useState<boolean>(false);
+
+  async function translateComment(cmt: string) {}
 
   return (
     <Flex
@@ -20,80 +26,104 @@ const Comment: NextPage<CommentProps> = ({ comments }) => {
       p={8}
       background="blackAlpha.800"
     >
-      <Stack direction="row" mb={8} alignItems="center">
-        <Link
-          href={{
-            pathname: `/`,
-          }}
-        >
-          <Button>메인 페이지</Button>
-        </Link>
-        <Button
-          onClick={() => {
-            const csv = stringify(
-              cleanText
-                ? comments.map((cmt) => {
-                    cmt.text = cmt.text.replace(/\s+/g, " ").trim();
-                    return cmt;
-                  })
-                : comments,
-              { header: true }
-            );
-            const blob = new Blob([csv], {
-              type: "text/csv;charset=utf-8;",
-            });
-            FileSaver.saveAs(blob, "data.csv");
-          }}
-        >
-          CSV 다운로드
-        </Button>
+      <Stack direction="column" alignItems="center" mb={8}>
+        <Stack direction="row" padding="4" alignItems="center">
+          <Link
+            href={{
+              pathname: `/`,
+            }}
+          >
+            <Button>메인 페이지</Button>
+          </Link>
+          <Button
+            onClick={() => {
+              const csv = stringify(
+                cleanText
+                  ? comments.map((cmt) => {
+                      cmt.text = cmt.text.replace(/\s+/g, " ").trim();
+                      return cmt;
+                    })
+                  : comments,
+                { header: true }
+              );
+              const blob = new Blob([csv], {
+                type: "text/csv;charset=utf-8;",
+              });
+              FileSaver.saveAs(blob, "data.csv");
+            }}
+          >
+            CSV 다운로드
+          </Button>
+          <Checkbox
+            color="white"
+            isChecked={cleanText}
+            onChange={(event) => {
+              setCleanText(event.target.checked);
+            }}
+          >
+            다운로드 시 댓글 엔터 정리
+          </Checkbox>
+        </Stack>
         <Checkbox
           color="white"
-          isChecked={cleanText}
+          isChecked={blurAvatar}
           onChange={(event) => {
-            setCleanText(event.target.checked);
+            setBlurAvatar(event.target.checked);
           }}
         >
-          다운로드 시 댓글 엔터 정리
+          아바타 블러
+        </Checkbox>
+        <Checkbox
+          color="white"
+          isChecked={hideName}
+          onChange={(event) => {
+            setHideName(event.target.checked);
+          }}
+        >
+          이름 가리기
+        </Checkbox>
+        <Checkbox
+          color="white"
+          isChecked={hideDate}
+          onChange={(event) => {
+            setHideDate(event.target.checked);
+          }}
+        >
+          날짜 가리기
         </Checkbox>
       </Stack>
+
       <Flex direction="column">
         {comments.map((cmt, i) => {
           return (
-            <Flex
-              key={i}
-              direction="row"
-              justify="space-between"
-              alignItems="flex-start"
-            >
-              <Flex gap={4} mr={12} my={5}>
-                <Avatar size="md" mt={2} src={cmt.profileImageUrl} />
-                <Stack direction="column">
-                  <Stack direction="row" alignItems="flex-end">
+            <Flex key={i} gap={4} mr={12} my={5}>
+              <Avatar
+                size="md"
+                mt={2}
+                src={cmt.profileImageUrl}
+                filter={blurAvatar ? `blur(${6}px)` : ""}
+              />
+              <Stack direction="column">
+                <Stack direction="row" alignItems="flex-end">
+                  {!hideName && (
                     <Text fontSize="xl" color="white">
                       {cmt.displayName}
                     </Text>
+                  )}
+                  {!hideDate && (
                     <Text fontSize="sm" color="whiteAlpha.700">
                       {cmt.publishAt}
                     </Text>
-                  </Stack>
-                  <Text fontSize="lg" color="white" whiteSpace="pre-line">
-                    {cmt.text}
-                  </Text>
-                  <Stack direction="row" alignItems="center">
-                    <TriangleUpIcon color="whiteAlpha.600" />
-                    <Text color="whiteAlpha.800">{cmt.likeCount}</Text>
-                    <TriangleDownIcon color="whiteAlpha.600" />
-                  </Stack>
+                  )}
                 </Stack>
-              </Flex>
-              <Stack direction="column" alignItems="center" mt={4}>
-                <Button size="sm" disabled={true}>
-                  이름 복사
-                </Button>
-                <Button size="sm" disabled={true}>
-                  댓글 복사
-                </Button>
+                <Text fontSize="lg" color="white" whiteSpace="pre-line">
+                  {cmt.text}
+                </Text>
+                <Stack direction="row" alignItems="center">
+                  <TriangleUpIcon color="whiteAlpha.600" />
+                  <Text color="whiteAlpha.800">{cmt.likeCount}</Text>
+                  <TriangleDownIcon color="whiteAlpha.600" />
+                </Stack>
               </Stack>
             </Flex>
           );
